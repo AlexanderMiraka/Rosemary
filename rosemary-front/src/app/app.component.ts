8ac74e191fc592250147551fad9b8e9800528ae4
hama1334 <hama1334@outlook.com.gr>
2025-07-21 15:32:24 +0300

    Connected frontend->backend->database

--- rosemary-front/src/app/app.component.ts
+++ rosemary-front/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './users/users.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rosemary';
+  users: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe((data: any) => {
+      this.users = data;
+    });
+  }
 }
