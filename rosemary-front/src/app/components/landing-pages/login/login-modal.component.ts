5468febc85037d96ac6e4e3d8152e586195a72a1
hama1334 <hama1334@outlook.com.gr>
2025-09-01 15:20:17 +0300

    add login fields

--- rosemary-front/src/app/components/landing-pages/login/login-modal.component.ts
+++ rosemary-front/src/app/components/landing-pages/login/login-modal.component.ts
@@ -1,33 +1,41 @@
-import { Component, Input, Output,EventEmitter, } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { registerUser } from "../../utils/fucntions/calls";
+import { registerUser } from '../../utils/fucntions/calls';
 
-@Component ({
-    selector: "login-modal",
-    templateUrl: "./login-modal.component.html",
-    styleUrls: ["./login-modal.component.scss"],
-    imports: [FormsModule],
-    standalone: true,
+@Component({
+  selector: 'login-modal',
+  templateUrl: './login-modal.component.html',
+  styleUrls: ['./login-modal.component.scss'],
+  imports: [FormsModule],
+  standalone: true,
 })
 export class loginModal {
-    @Output() closeModal = new EventEmitter<boolean>();//passes false to parent when modal closes
-    close() {
-        this.closeModal.emit(false);
+  @Output() closeModal = new EventEmitter<boolean>(); //passes false to parent when modal closes
+  close() {
+    this.closeModal.emit(false);
+  }
+  modalShow: String = 'login';
+  toggleModal(toogle: String): String {
+    this.modalShow = toogle;
+    return this.modalShow;
+  }
+  registerUser = {
+    username: '',
+    email: '',
+    password: '',
+    mobile: '',
+    biography: '',
+  };
+  loginUser = {
+    email: '',
+    password: '',
+  };
+  formSubmit(form: any, mode: String) {
+    if (mode === 'register') {
+      registerUser(this.registerUser);
+      this.close();
+    } else if (mode === 'login') {
+      this.close();
     }
-    modalShow: String = "login";
-    toggleModal(toogle: String) : String{
-        this.modalShow = toogle;
-        return this.modalShow;
-    }
-    registerUser = {
-        username: "",
-        email: "",
-        password: "",
-        mobile:"",
-        biography: "",
-    }
-    formSubmit(form: any) {
-        registerUser(this.registerUser);
-        this.close();
-    }
+  }
 }
