8bfe9f3e71dfb056f06b9d1e77f269c21ff204c3
hama1334 <hama1334@outlook.com.gr>
2025-10-06 14:20:35 +0300

    Add first step of recipe creator modal

--- rosemary-front/src/app/components/landing-pages/modals/recipe-modal.component.ts
+++ rosemary-front/src/app/components/landing-pages/modals/recipe-modal.component.ts
@@ -1,16 +1,44 @@
-import { Component, signal, EventEmitter, Output } from "@angular/core";
-
+import { Component, signal, EventEmitter, Output, Signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
-    templateUrl: "./recipe-modal.component.html",
-    styleUrl: "./recipe-modal.component.scss",
-    selector: 'recipe-modal',
-    standalone: true,
+  templateUrl: './recipe-modal.component.html',
+  styleUrl: './recipe-modal.component.scss',
+  selector: 'recipe-modal',
+  standalone: true,
+  imports: [FormsModule],
 })
-
 export class RecipeModal {
-    constructor() {}
-    @Output() modalIsOpen = new EventEmitter<void>();
-    closeModal() {
-        this.modalIsOpen.emit();
-    }
+  constructor() {}
+  @Output() modalIsOpen = new EventEmitter<void>();
+  closeModal() {
+    this.modalIsOpen.emit();
+    this.setIngredientsTable('');
+  }
+  measurements: String[] = [
+    'Count (No.)',
+    'Teaspoon (tsp)',
+    'Tablespoon (tbsp)',
+    'Cup (c)',
+    'Fluid ounce (fl oz)',
+    'Milliliter (mL)',
+    'Liter (L)',
+    'Ounce (oz)',
+    'Pound (lb)',
+    'Gram (g)',
+    'Kilogram (kg)',
+    'Pinch',
+    'Dash',
+    'Handful',
+  ];
+  ingredients = signal<string[]>([]);
+  setIngredientsTable(lastIngredient: any) {
+    this.ingredients.set([...this.ingredients(),lastIngredient]);
+  }
+  ingredient = '';
+  quantity = '';
+  measurement = '';
+  addIngredient = ()=>{
+    const ingredient = `${this.quantity} ${this.measurement} ${this.ingredient}`;
+    this.setIngredientsTable(ingredient);
+  } 
 }
